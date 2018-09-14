5811119ff4231c5af09f184ee26bcde5054e2113
Roland Oechslin <ro@garaio.com>
2018-09-14 06:18:09 +0200

    add get external users

---
+++ src/scripts/get-externalUsers.ps1
@@ -0,0 +1,23 @@
+# https://absolute-sharepoint.com/2018/03/create-a-report-of-sharepoint-online-external-users-with-powershell.html
+
+$SiteCollections = Get-SPOSite -Limit All
+foreach ($site in $SiteCollections) {
+    try {
+        for ($i = 0; ; $i += 50) {
+
+            $ExternalUsers += Get-SPOExternalUser -SiteUrl $site.Url -PageSize 50 -Position $i -ea Stop | Select DisplayName, EMail, AcceptedAs, WhenCreated, InvitedBy, @{Name = "Url" ; Expression = { $site.url } }
+
+            # ShowOnlyUsersWithAcceptingAccountNotMatchInvitedAccount
+            # It shows you all the external accounts that have been invited to your tenant using an e-mail address, 
+            # but they have accepted the invitation and are using a different e-mail address to authenticate to your SharePoint Online.
+            # $ExternalUsers += Get-SPOExternalUser -ShowOnlyUsersWithAcceptingAccountNotMatchInvitedAccount $true -PageSize 50 -Position $i  -ea Stop
+
+        }
+    }
+    catch {
+    }
+}
+
+$ExternalUsers | Export-Csv -Path "C:\temp\ExternalUsersPerSC.csv" -NoTypeInformation
+
+
