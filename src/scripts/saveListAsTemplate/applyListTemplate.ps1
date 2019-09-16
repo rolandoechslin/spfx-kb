29e51f7387e30e579fb283f0eda61c74c9fa8913
Roland Oechslin <ro@garaio.com>
2019-09-16 06:23:32 +0200

    add save as template script

---
+++ src/scripts/saveListAsTemplate/applyListTemplate.ps1
@@ -0,0 +1,12 @@
+﻿$siteURL =  "https://contoso.sharepoint.com/sites/hr"
+$templateName = "listTemplate"
+
+$path = [regex]::Replace($MyInvocation.MyCommand.Definition, "\\applyListTemplate.ps1", "")
+cd $path
+
+Connect-PnPOnline -Url $siteURL
+
+Apply-PnPProvisioningTemplate -Path ("{0}.xml" -f $templateName)
+
+
+
---
+++ src/scripts/saveListAsTemplate/saveListTemplate.ps1
@@ -0,0 +1,11 @@
+﻿$siteURL = "https://contoso.sharepoint.com/sites/hr"
+$listName = "listName"
+$templateName = "listTemplate"
+
+$path = [regex]::Replace($MyInvocation.MyCommand.Definition, "\\saveListTemplate.ps1", "")
+cd $path
+
+Connect-PnPOnline -Url $siteURL
+Get-PnPProvisioningTemplate -Handlers Lists -ListsToExtract $listName -Out ("{0}.xml" -f $templateName)
+Add-PnPDataRowsToProvisioningTemplate -path ("{0}.xml" -f $templateName) -List $listName -Query '<view></view>'
+
