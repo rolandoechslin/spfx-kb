294a2c50cc7c8042f374cefdc92f966b2b604027
Roland Oechslin <ro@garaio.com>
2022-03-20 11:01:13 +0100

    add report

--- src/scripts/html-reports-framework/PSWriteHTML.test.ps1
+++ src/scripts/html-reports-framework/PSWriteHTML.test.ps1
@@ -2,6 +2,13 @@
 # https://github.com/EvotecIT/PSWriteHTML/blob/master/Examples/Example-DashimoStyle/Run-EasyDashboard.ps1
 # https://github.com/T0pCyber/hawk
 # https://github.com/KelvinTegelaar/CIPP-API
+# https://github.com/21bshwjt/pki-polaris
+# https://github.com/msp4msps/Security/tree/25fdfb34a97f762ee6af46e70ad9936a0b318cff
+# https://github.com/msp4msps/Syncro-Documentation
+# https://github.com/shiftnerd/Office365UserLicenseReport/blob/f1ae225b1fab51984b4e67df6a51b5dddb9530e1/emaillicensereport-cleaned.ps1
+# https://github.com/EvotecIT/O365Essentials/blob/ab07bdd1c7c7179868ee23cee9bcf62d15b3d11f/COMMANDS.MD
+# https://github.com/EvotecIT/PSTeams
+# https://evotec.xyz/office-365-health-service-using-powershell/
 
 Install-Module -Name PSWriteHTML
 
