4210b9f05b3924d37b21bfc35dc5eca2ef550499
Roland Oechslin <ro@garaio.com>
2023-03-21 10:53:39 +0100

    Deployed 9a5eeefe with MkDocs version: 0.17.3

--- spfx/ichicraft.ps1
+++
@@ -1,5 +0,0 @@
-# Ichicraft 
-
-## News Personalization in SPO
-
-- [SPO possibiities in creating and share news](https://support.microsoft.com/en-us/office/create-and-share-news-on-your-sharepoint-sites-495f8f1a-3bef-4045-b33a-55e5abe7aed7)
